bdc7d0ab45bb032b3de0a372fe593e194016c925
Emanalytics7 <emannisar777@gmail.com>
2024-04-10 02:11:34 +0500

    stage[0] DATA INGESTION

--- src/pipeline/stage_01_ingestion.py
+++ src/pipeline/stage_01_ingestion.py
@@ -1,11 +1,31 @@
-import os 
-import zipfile
 import logging
+import os
+import zipfile
+import csv
+from dataclasses import dataclass
+from src.utils.logger import setup_logger
 
-logging.basicConfig(filename='stage_01_data_ingestion.py', level=logging.INFO,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
+logger = setup_logger()
+
+@dataclass
+class DataIngestionConfig:
+    zip_file_path: str
+    log_file_path: str
+    output_dir: str
 
 class DataIngestion:
-    def __init__(self):
-        
-def extract_zip(zip_path)
+    def __init__(self, config):
+        self.config = config
+        self.logger = logger
+    
+    def extract_zip_file(self):
+        try:
+            with zipfile.ZipFile(self.config.zip_file_path, 'r') as zip_ref:
+                zip_ref.extractall(self.config.output_dir)
+            self.logger.info("Zip file extracted successfully!")
+        except Exception as e:
+            self.logger.error(f"Error in extracting zip file: {e}")
+
+config = DataIngestionConfig(zip_file_path='data/processed_data.zip', log_file_path='logs', output_dir='data/')
+data_ingestion = DataIngestion(config)
+data_ingestion.extract_zip_file()
