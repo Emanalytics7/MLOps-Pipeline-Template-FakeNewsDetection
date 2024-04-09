36f5e5eb28b34d4ff7a4888ff4d761c990a9ee95
Emanalytics7 <emannisar777@gmail.com>
2024-04-10 00:02:06 +0500

    Update data_ingestion.py

---
+++ src/pipeline/stage_01_ingestion.py
@@ -0,0 +1,11 @@
+import os 
+import zipfile
+import logging
+
+logging.basicConfig(filename='stage_01_data_ingestion.py', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+class DataIngestion:
+    def __init__(self):
+        
+def extract_zip(zip_path)
