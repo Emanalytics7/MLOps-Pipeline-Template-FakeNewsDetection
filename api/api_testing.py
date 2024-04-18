d0e03c562e2f56247550fae60bc3f8c063a8a8ca
Emanalytics7 <emannisar777@gmail.com>
2024-04-18 06:21:46 +0500

    Created README.md

--- api/api_testing.py
+++ api/api_testing.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/predict' # local host you can adjust yours here!
+url = 'http://52.172.25.57:5000/predict' 
 data = {'text': 'A new study reveals that eating chocolate every day can make you lose weight without exercise.'}
 response = requests.post(url, json=data)
 print(response.text)
