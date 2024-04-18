146dde0c892527617c8bdadaf5dd67b484659bd4
Emanalytics7 <emannisar777@gmail.com>
2024-04-18 06:25:09 +0500

    UPDATED api-testing

--- api/api_testing.py
+++ api/api_testing.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://52.172.25.57:5000/predict' 
+url = 'http://52.172.25.57:5000/predict' # Replace with your IP address or url of your deployed api
 data = {'text': 'A new study reveals that eating chocolate every day can make you lose weight without exercise.'}
 response = requests.post(url, json=data)
 print(response.text)
